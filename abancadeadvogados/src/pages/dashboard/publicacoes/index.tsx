0a8e8db1172cdfd58240f97680f279f6c3e9d559
Gustavo Castro <gustavohecastro@gmail.com>
2023-02-21 11:31:02 -0300

    fix boolean issue

--- abancadeadvogados/src/pages/dashboard/publicacoes/index.tsx
+++ abancadeadvogados/src/pages/dashboard/publicacoes/index.tsx
@@ -16,7 +16,7 @@ async function getAllPosts(){
     
     const posts = await prisma.post.findMany({
       where:{
-        isActive : true
+        isActive : 1
       },
       include : {
           userId: {
