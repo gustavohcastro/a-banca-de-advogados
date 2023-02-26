415728191f13b594f36010d5aba2369aa9aaefc2
Gustavo Castro <gustavohecastro@gmail.com>
2023-02-26 00:25:46 -0300

    add cors to apis

--- abancadeadvogados/src/pages/api/posts/delete-post.ts
+++ abancadeadvogados/src/pages/api/posts/delete-post.ts
@@ -1,10 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Post } from './interface'
-import prisma from "../../../lib/prismadb"
+import prisma from "../../../lib/prismadb";
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        
+        await runMiddleware(req, res, cors);
         const data = req.body;
         
         if (!data.postId) {
--- abancadeadvogados/src/pages/api/posts/new-post.ts
+++ abancadeadvogados/src/pages/api/posts/new-post.ts
@@ -1,10 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
 import prisma from "../../../lib/prismadb"
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {  
-        
+        await runMiddleware(req, res, cors);
         const result = await prisma.post.create({data: req.body})
         res.status(200).json({post: result.id})
     }
