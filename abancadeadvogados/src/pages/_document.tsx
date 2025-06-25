8074a84901c9fe50fe70767e8d84231453630d70
Gustavo Castro <gustavohecastro@gmail.com>
2025-06-25 16:02:26 -0300

    add GTM

--- abancadeadvogados/src/pages/_document.tsx
+++ abancadeadvogados/src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, {DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript} from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import React from 'react'
 
@@ -28,17 +28,37 @@ export default class MyDocument extends Document {
         return (
             <Html lang='pt'>
                 <Head>
-                    <meta charSet="utf-8"/>
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;800&display=swap" rel="stylesheet"/>
+                    <meta charSet="utf-8" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;800&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap" rel="stylesheet"></link>
                     <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+
+                    {/* Google Tag Manager */}
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-54BJ5ZQR');`
+                        }}
+                    />
                 </Head>
                 <body>
-                    <Main/>
-                    <NextScript/>
+                    {/* Google Tag Manager (noscript) */}
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-54BJ5ZQR"
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        ></iframe>
+                    </noscript>
+                    <Main />
+                    <NextScript />
                 </body>
             </Html>
         )
